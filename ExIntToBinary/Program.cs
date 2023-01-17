5145bb313767c8e81628572eefddfc5c231f5864
andrew_nizovkin <nizovkinandrej@gmail.com>
2023-01-17 09:34:32 +0300

    Initial commit

---
+++ ExIntToBinary/Program.cs
@@ -0,0 +1,16 @@
+﻿int number = 0;
+string binary = "";
+if (number != 0)
+{
+    while (number > 0)
+    {
+        binary = number % 2 + binary;
+        number = number / 2;
+    }
+}
+else
+{
+    binary = "0";
+}
+Console.WriteLine(binary);
+
---
+++ ExOne/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое целое число");
+int firstNumber = System.Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе целое число");
+int secondNumber = System.Convert.ToInt32(Console.ReadLine());
+
+if ((Math.Pow(firstNumber, 2)) == secondNumber)
+{
+    Console.WriteLine("Второе число является квадратом первого");
+}
+
+else
+{
+    Console.WriteLine("Второе число не является квадратом первого");
+}
+
+
